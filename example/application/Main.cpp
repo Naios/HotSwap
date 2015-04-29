3be8fc2c630f149439bf05acf8f6958cedb9cac9
Naios <naios-dev@live.de>
2015-04-29 18:53:43 +0200

    Add some missing includes and link libs

--- example/application/Main.cpp
+++ example/application/Main.cpp
@@ -1,5 +1,10 @@
 
+#include <boost/filesystem.hpp>
+
+#include "Module.hpp"
+
 int main(int argc, char** argv)
 {
+    boost::filesystem::path p;
     return 0;
 }
---
+++ src/Module.hpp
@@ -0,0 +1,5 @@
+
+#ifndef Module_hpp_
+#define Module_hpp_
+
+#endif Module_hpp_
